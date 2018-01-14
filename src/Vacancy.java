463da3aab0fa1fb55c3a19512ad01dccaa651088
Veesve <peterIKBO@gmail.com>
2018-01-14 08:40:17 +0300

    upload files

---
+++ src/Vacancy.java
@@ -0,0 +1,17 @@
+public class Vacancy {
+    private String companyName;
+    private String salaryCount;
+    private String creatingDate;
+    private String title;
+    private String url;
+    private String address;
+
+    public Vacancy(String companyName, String salaryCount, String creatingDate, String title, String url, String address) {
+        this.companyName = companyName;
+        this.salaryCount = salaryCount;
+        this.creatingDate = creatingDate;
+        this.title = title;
+        this.url = url;
+        this.address = address;
+    }
+}
